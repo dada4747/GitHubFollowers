689bc8cc96b989a0d99463c9e010809d083ade45
Rahul Adsure <adsurerahul96@gmail.com>
2022-04-22 04:06:41 +0530

    ADDED: App Navigation Setup, UITabBarController, UINavigationController

---
+++ GitHubFollowers/FavoritesListVC.swift
@@ -0,0 +1,19 @@
+//
+//  FavoritesListVC.swift
+//  GitHubFollowers
+//
+//  Created by admin on 22/04/22.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+    
+
+
+}
---
+++ GitHubFollowers/SearchVC.swift
@@ -0,0 +1,21 @@
+//
+//  SearchVC.swift
+//  GitHubFollowers
+//
+//  Created by admin on 22/04/22.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBlue
+    }
+    
+
+    
+
+}
