461df538b1d7ddb37a81dd26d7b47fa0cd66aa6a
Rahul Adsure <adsurerahul96@gmail.com>
2022-04-23 17:28:00 +0530

    REFACTORED: Network call with result type for network call  handling failure and success

--- GitHubFollowers/Managers/NetworkManager.swift
+++ GitHubFollowers/Managers/NetworkManager.swift
@@ -14,14 +14,14 @@ class NetworkManager {
     
     private init() {}
     
-    func getFollowers(for username: String, page: Int, completed: @escaping ([Follower]?, ErrorMessage?) ->Void ) {
+    func getFollowers(for username: String, page: Int, completed: @escaping (Result<[Follower], GFError>) ->Void ) {
         let endPoint = baseURL + "\(username)/followers?per_page=100&page=\(page)"
         
         // make sure valid url
         guard let url = URL(string: endPoint) else {
             
             // - if does'nt return valid url
-            completed(nil, .invalidData)
+            completed(.failure(.invalidUsername))
             return
         }
         
@@ -30,18 +30,18 @@ class NetworkManager {
             
             // - handle the error
             if let _ = error {
-                completed(nil, .unableToComplete)
+                completed(.failure(.unableToComplete))
             }
             
             // - handle the response
             guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
-                completed(nil, .invalidResponse)
+                completed(.failure(.invalidResponse))
                 return
             }
             
             // - store response into data
             guard let data = data else {
-                completed(nil, .invalidData)
+                completed(.failure(.invalidData))
                 return
             }
             
@@ -52,9 +52,9 @@ class NetworkManager {
                 decoder.keyDecodingStrategy = .convertFromSnakeCase
                 // - decode and create array of follower from data
                 let followers = try decoder.decode([Follower].self, from: data)
-                completed(followers, nil)
+                completed(.success(followers))
             } catch {
-                completed(nil, .invalidData)
+                completed(.failure(.invalidData))
             }
         }
         task.resume()
