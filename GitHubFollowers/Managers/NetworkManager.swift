95c9d79ae2db8a88de8cb8ced6c3e07ffbf909a8
Rahul Adsure <adsurerahul96@gmail.com>
2022-04-23 20:00:44 +0530

    ADDED: Custom Followers Cell, GFAvatarImageView, Configure FollowersListVC

--- GitHubFollowers/Managers/NetworkManager.swift
+++ GitHubFollowers/Managers/NetworkManager.swift
@@ -9,47 +9,46 @@ import Foundation
 
 class NetworkManager {
     
-    static let shared = NetworkManager()
-    let baseURL = "https://api.github.com/users/"
+    static let shared   = NetworkManager()
+    let baseURL         = "https://api.github.com/users/"
     
     private init() {}
     
     func getFollowers(for username: String, page: Int, completed: @escaping (Result<[Follower], GFError>) ->Void ) {
         let endPoint = baseURL + "\(username)/followers?per_page=100&page=\(page)"
         
-        // make sure valid url
+        // TODO: - Check URL validation
         guard let url = URL(string: endPoint) else {
-            
-            // - if does'nt return valid url
             completed(.failure(.invalidUsername))
             return
         }
         
-        // - url session data task to actually get data task from url
+        // TODO: - Request URL using URLSession
         let task = URLSession.shared.dataTask(with: url) { data, response, error in
-            
-            // - handle the error
+        
+            // TODO: - Then inside the dataTask completion, we will handle the error and success data
             if let _ = error {
                 completed(.failure(.unableToComplete))
             }
             
-            // - handle the response
             guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
                 completed(.failure(.invalidResponse))
                 return
             }
             
-            // - store response into data
             guard let data = data else {
                 completed(.failure(.invalidData))
                 return
             }
+            
+            // TODO: - Decode data in try block
             
             do {
                 let decoder = JSONDecoder()
                 
                 // convert from snak case to camelcase / object
                 decoder.keyDecodingStrategy = .convertFromSnakeCase
+            
                 // - decode and create array of follower from data
                 let followers = try decoder.decode([Follower].self, from: data)
                 completed(.success(followers))
--- GitHubFollowers/Screens/FollowersListVC.swift
+++ GitHubFollowers/Screens/FollowersListVC.swift
@@ -8,15 +8,56 @@
 import UIKit
 
 class FollowersListVC: UIViewController {
-   
+    
     var userName: String!
+    
+    // MARK: Declare variable for collection view
+    var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        configureViewController()
+        configureCollectionView()
+        getFollowers()
+       
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+    
+    // MARK: - Configure view controller
+    func configureViewController() {
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    // MARK: - Configure collection view
+    func configureCollectionView() {
+        //initialize collection view
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createThreeColumnFlowLayout())
         
+        view.addSubview(collectionView)
+        collectionView.backgroundColor = .systemBackground
+        collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
+    }
+    
+    // MARK: - Create three column
+    func createThreeColumnFlowLayout () -> UICollectionViewFlowLayout {
+        let width                       = view.bounds.width
+        let padding: CGFloat            = 12
+        let minItemSpacing : CGFloat    = 10
+        let availableWidth              = width - (padding * 2 ) - (minItemSpacing * 2)
+        let itemWidth                   = availableWidth / 3
+        
+        let flowLayout                  = UICollectionViewFlowLayout()
+        flowLayout.sectionInset         = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        flowLayout.itemSize             = CGSize(width: itemWidth, height: itemWidth)
+        return flowLayout
+    }
+    
+    // MARK: - get followers network call
+    func getFollowers() {
         NetworkManager.shared.getFollowers(for: userName, page: 1) { result in
             
             switch result {
@@ -26,12 +67,8 @@ class FollowersListVC: UIViewController {
             case .failure(let error):
                 self.presentGFAlertOnMainThread(title: "Bad Stuff Happend", message: error.rawValue, buttonTitle: "Ok")
             }
-     
+            
         }
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        navigationController?.setNavigationBarHidden(false, animated: true)
-    }
 }
