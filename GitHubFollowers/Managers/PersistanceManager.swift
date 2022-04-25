86ff74283bd2985a8aeede397cfd10b4c6c6d504
Rahul Adsure <adsurerahul96@gmail.com>
2022-04-25 21:54:48 +0530

    ADDED: Persistence Manager to add favourites and retrieve favourite

---
+++ GitHubFollowers/Managers/PersistanceManager.swift
@@ -0,0 +1,74 @@
+//
+//  PersistanceManager.swift
+//  GitHubFollowers
+//
+//  Created by admin on 25/04/22.
+//
+
+import Foundation
+
+enum PersistenceActionType {
+    case add, remove
+}
+
+enum PersistanceManager {
+    static private let defaults = UserDefaults.standard
+    
+    enum Keys {
+        static let favorites = "favorites"
+    }
+    //MARK: - update data with follower remove, update
+    static func updateWith(favorite: Follower, actionType: PersistenceActionType, completed: @escaping(GFError?) -> Void) {
+        retriveFavorites { result in
+            switch result {
+            case .success(let favorites):
+                var retrivedFavorites = favorites
+                switch actionType {
+                case .add:
+                    //this make sure this doesnt contain favorite already
+                    guard !retrivedFavorites.contains(favorite) else {
+                        completed(.alreadyInFavorites)
+                        return
+                    }
+                    retrivedFavorites.append(favorite)
+                case .remove:
+                    retrivedFavorites.removeAll {
+                        $0.login == favorite.login
+                    }
+                }
+                completed(save(favorites: retrivedFavorites))
+                
+            case .failure(let error):
+                completed(error)
+            }
+        }
+    }
+    
+    // MARK: - retrive favorites
+    static func retriveFavorites(completed: @escaping (Result<[Follower], GFError>) -> Void) {
+        guard let favoritesData = defaults.object(forKey: Keys.favorites) as? Data else {
+            completed(.success([]))
+            return
+        }
+        
+        do {
+            let decoder = JSONDecoder()
+            let favorites = try decoder.decode([Follower].self, from: favoritesData)
+            completed(.success(favorites))
+        } catch {
+            completed(.failure(.unableToFavorite))
+        }
+    }
+    
+    //MARK: - save favorites
+    static func save(favorites: [Follower]) -> GFError? {
+        do {
+            let encoder = JSONEncoder()
+            let encodedFavorites = try encoder.encode(favorites)
+            defaults.set(encodedFavorites, forKey: Keys.favorites)
+            return nil
+        } catch {
+            return .unableToFavorite
+        }
+    }
+}
--- GitHubFollowers/Screens/FavoritesListVC.swift
+++ GitHubFollowers/Screens/FavoritesListVC.swift
@@ -12,6 +12,16 @@ class FavoritesListVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemPink
+        
+        PersistanceManager.retriveFavorites { result in
+            switch result {
+            case .success(let favorites):
+                print(favorites)
+                
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
     
 
